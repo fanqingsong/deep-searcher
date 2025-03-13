d02bfe19274d416487861a90b17e7e1a9080c197
cnJasonZ <gbdzxalbb@qq.com>
2025-03-13 21:12:31 +0800

    feat: add PPIO embeddings

--- deepsearcher/embedding/__init__.py
+++ deepsearcher/embedding/__init__.py
@@ -4,6 +4,7 @@ from .milvus_embedding import MilvusEmbedding
 from .openai_embedding import OpenAIEmbedding
 from .siliconflow_embedding import SiliconflowEmbedding
 from .voyage_embedding import VoyageEmbedding
+from .ppio_embedding import PPIOEmbedding
 
 __all__ = [
     "MilvusEmbedding",
@@ -12,4 +13,5 @@ __all__ = [
     "BedrockEmbedding",
     "SiliconflowEmbedding",
     "GeminiEmbedding",
+    "PPIOEmbedding",
 ]
